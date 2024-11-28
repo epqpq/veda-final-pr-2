8c6940ef5c1256c13ad804865354a6f5cc2ab130
강민경 <106790405+minkyung32@users.noreply.github.com>
2024-11-28 16:01:39 +0900

    Add function on server
    클라이언트와 통신 기능 추가
     - 클라이언트로부터 감지 기능 on/off 신호를 받을 수 있음
      --> 받는 신호에 따라 기능을 켜고 끌 수 있도록 함
     - 클라이언트에게 감지되었음을 알리는 신호를 보낼 수 있음

--- cm_server/camera_server.h
+++ cm_server/camera_server.h
@@ -6,12 +6,24 @@
 #include <gst/rtsp-server/rtsp-server.h>
 #include <string>
 #include <iostream>
+#include <thread>
+#include <cstring>
+#include <sstream>
+#include <vector>
+#include <chrono>
+#include <atomic>
 
 #include <sys/types.h>
 #include <ifaddrs.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
+
+using namespace std;
+using namespace chrono;
+
+#define TCP_PORT 5100
 
 class cServer
 {
@@ -19,14 +31,34 @@ private:
     std::string pipeline;
     cv::VideoCapture cap;
     cv::VideoWriter videoWriter;
-
     std::string getIp();
+    vector<string> flagv;
+    istringstream ss;
+    string temp;
+
+    time_point<steady_clock> now_t;
+    time_point<steady_clock> motion_t = steady_clock::time_point::min(); //최소시간으로 초기화
+    time_point<steady_clock> fire_t = steady_clock::time_point::min();
+
+    //for tcp
+    int ssock, csock, readstr;
+    socklen_t clen;
+    struct sockaddr_in servaddr, cliaddr;
+    char cIP[BUFSIZ];
+    char mesg[BUFSIZ];
+    const char* noticeMotion = "1;motion";
+    const char* noticeFire = "1;fire";
 public:
+    atomic<int> tcpFlag = 0; //통신을 위한 플래그
+    atomic<bool> power_motion = true;
+    atomic<bool> power_fire = true;  //기능 on/off를 위한 플래그
+    thread tcpThread;
     cServer();
     ~cServer();
     int init();
     cv::Mat getSource();
     int setSource(cv::Mat);
+    void tcpCommunication();
 };
 
 #endif
