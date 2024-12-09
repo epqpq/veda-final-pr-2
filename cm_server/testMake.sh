1350d1f87fb0d147a7073b5fedddc6dfb26390d3
JeMinMoon <100738519+JeMinMoon@users.noreply.github.com>
2024-12-09 14:00:46 +0900

    Delete cm_server/testMake.sh
    Makefile 작성해서 이제 필요없음

--- cm_server/testMake.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-##g++ -o main camera_server.cpp camera_server_main.cpp `pkg-config --libs --cflags opencv4 gstreamer-1.0 gstreamer-app-1.0 gstreamer-rtsp-server-1.0`
-
-g++ -o main camera_server.cpp camera_server_main.cpp `pkg-config --libs --cflags opencv4 gstreamer-1.0 gstreamer-app-1.0 gstreamer-rtsp-server-1.0` -L/home/pi/flatbuffers/build -lflatbuffers -ltensorflow-lite
