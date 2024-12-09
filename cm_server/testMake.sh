a7c561b793b1b82366a45def6c9e59e1ba02950b
JeMinMoon <100738519+JeMinMoon@users.noreply.github.com>
2024-12-09 14:47:03 +0900

    Delete cm_server/testMake.sh

--- cm_server/testMake.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-##g++ -o main camera_server.cpp camera_server_main.cpp `pkg-config --libs --cflags opencv4 gstreamer-1.0 gstreamer-app-1.0 gstreamer-rtsp-server-1.0`
-
-g++ -o main camera_server.cpp camera_server_main.cpp motionDiff.cpp fire_detector.cpp `pkg-config --libs --cflags opencv4 gstreamer-1.0 gstreamer-app-1.0` -ltensorflow-lite -lssl -lcrypto
